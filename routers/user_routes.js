7000201267ba411aca1a82dbf73d34826769fbe1
Huishan <hui_shan91@hotmail.com>
2022-10-19 16:58:16 +0800

    feat: added controller and routes files

---
+++ routers/user_routes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const userController = require("../controllers/users/user_controller");
+const bookingController = require("../controllers/bookings/booking_controller");
+// const listingController = require("../controllers/listings/listing_controller");
+// const validation = require("../middlewares/validation/validation");
+// const listingValidators = require("../middlewares/validation/validators/listingValidators");
+// const userValidators = require("../middlewares/validation/validators/userValidators");
+// const authMiddleware = require("../middlewares/authorization/authmiddleware");
+// const imageMethods = require("../middlewares/uploadImage/uploadImage");
+// const multer = require("multer");
+// const storage = multer.memoryStorage()
+// const upload = multer({ storage: storage })
+// const upload = multer();
+
+//http://localhost:8000/api/v1/user
+const router = express.Router();
+
+router.post(
+  "/register",
+  validation(userValidators.register),
+  userController.register
+); // returns 201
+router.post("/login", validation(userValidators.login), userController.login); // returns 201
+router.post("/logout", userController.logout); // returns 201
+
+//add authMiddleware is used for any route that needs authentication
+// router.get('/profile', authMiddleware, userController.showProfile)//returns {}
+// router.patch('/profile', authMiddleware, userController.editProfile)// returns 201
+// router.delete('/profile', authMiddleware, userController.deleteProfile)// returns 201
+
+// router.get('/trips', authMiddleware, bookingController.showTrips)//returns []
+// router.get('/trip/:booking_id', authMiddleware, bookingController.showTrip)//returns []
+// router.patch('/trip/:booking_id', authMiddleware, bookingController.editTrip)// returns 201
+// router.delete('/trip/:booking_id', authMiddleware, bookingController.deleteTrip)// returns 201
+// router.post('/book/:listing_id', authMiddleware, bookingController.bookTrip)// returns 201
+
+//get,create, edit, delete each listing
+// router.get('/listings', authMiddleware, listingController.listHostListings)//returns []
+// router.get('/listing/:listing_id', authMiddleware, bookingController.showListingBookings)//returns []
+// router.post('/listing', authMiddleware, upload.any('files'), imageMethods.uploadImage, listingController.createListing)//return 201
+// router.patch('/listing/:listing_id', authMiddleware, listingController.editListing)// returns 201
+// router.delete('/listing/:listing_id', authMiddleware, listingController.deleteListing)// return 201
+//  upload.array('file', 12),upload.single("file")validation(listingValidators.createListing)
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const doctorRouter = require("./routers/doctor_routes");
+const bookingRouter = require("./routers/booking_routes");
 const userRouter = require("./routers/user_routes");
 
 const app = express();
@@ -20,16 +20,9 @@ app.use(
   })
 );
 
-//  main pg
-app.use("/api/v1");
-
-// routes belonging to doctor
-app.use("/api/v1/doctors", doctorRouter);
-
-// routes belonging to user
-app.use("/api/v1/user", userRouter);
-
-// app.use("/api/v1/appointments", appointmentRouter); -> show appointment for doctors
+// app.use("/api/v1");
+// app.use("/api/v1/doctors", bookingRouter);
+// app.use("/api/v1/user", userRouter);
 
 app.listen(process.env.PORT, async () => {
   console.log(`Heyy Doc backend listening on port ${process.env.PORT}`);
