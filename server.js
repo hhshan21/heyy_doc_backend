a77b7ff354419f59987280fc0018923916b0de56
Huishan <hui_shan91@hotmail.com>
2022-10-19 15:45:50 +0800

    feat: updated connection string with process.env variables

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-// const mongoose = require("mongoose");
+const Sequelize = require("sequelize");
 // const listingRouter = require("./routers/listing_routes");
 // const userRouter = require("./routers/user_routes");
 
 const app = express();
 const port = process.env.PORT || 8000;
-// const connStr = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_HOST}.nrg3u.mongodb.net/?retryWrites=true&w=majority`;
+const connStr = `postgres://${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DBNAME}`;
+// const db = new Sequelize(‘postgres://localhost:5432/petOwners’);
 
 app.get("/", (req, res) => {
   res.send("Heyy Doc!");
@@ -23,20 +24,18 @@ app.use(
   })
 );
 
-// app.use("/api/v1", listingRouter);
-// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1"); -> main pg
+// app.use("/api/v1/doctors", doctorRouter); -> show list of doctors
+// app.use("/api/v1/user", userRouter); -> make bookings with doctors, profile
+// app.use("/api/v1/appointments", appointmentRouter); -> show appointment for doctors
 
-app.listen(port, () => {
+app.listen(port, async () => {
+  try {
+    await connStr;
+  } catch (err) {
+    console.log(`Failed to connect to DB`);
+    process.exit(1);
+  }
+
   console.log(`Heyy Doc backend listening on port ${port}`);
 });
-
-// app.listen(port, async () => {
-//   try {
-//     await mongoose.connect(connStr, { dbName: process.env.MONGO_DB });
-//   } catch (err) {
-//     console.log(`Failed to connect to DB`);
-//     process.exit(1);
-//   }
-
-//   console.log(`Heyy Doc backend listening on port ${port}`);
-// });
