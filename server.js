15747f2329c6a433c92ebd4d770eb7acff9edc26
Huishan <hui_shan91@hotmail.com>
2022-10-19 16:12:13 +0800

    feat: revised connection string process.env

--- server.js
+++ server.js
@@ -2,14 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const Sequelize = require("sequelize");
 // const listingRouter = require("./routers/listing_routes");
 // const userRouter = require("./routers/user_routes");
 
 const app = express();
-const port = process.env.PORT || 8000;
-const connStr = `postgres://${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DBNAME}`;
-// const db = new Sequelize(‘postgres://localhost:5432/petOwners’);
 
 app.get("/", (req, res) => {
   res.send("Heyy Doc!");
@@ -29,13 +25,6 @@ app.use(
 // app.use("/api/v1/user", userRouter); -> make bookings with doctors, profile
 // app.use("/api/v1/appointments", appointmentRouter); -> show appointment for doctors
 
-app.listen(port, async () => {
-  try {
-    await connStr;
-  } catch (err) {
-    console.log(`Failed to connect to DB`);
-    process.exit(1);
-  }
-
-  console.log(`Heyy Doc backend listening on port ${port}`);
+app.listen(process.env.PORT, async () => {
+  console.log(`Heyy Doc backend listening on port ${process.env.PORT}`);
 });
