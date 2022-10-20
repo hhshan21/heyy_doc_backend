d431e20ddf643acea937508997b99609c75d5cf1
Huishan <hui_shan91@hotmail.com>
2022-10-20 19:03:04 +0800

    feat: seeded data

--- seeders/20221020102253-demo-booking.js
+++ seeders/20221020102253-demo-booking.js
@@ -6,8 +6,18 @@ module.exports = {
     await queryInterface.bulkInsert("bookings", [
       {
         patientId: 1,
-        doctorId: 
+        doctorId: 3,
         bookingAt: "2022/10/22",
+        startAt: "09:00",
+        endAt: "10:00",
+        symptoms: "fever, headache",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      },
+      {
+        patientId: 2,
+        doctorId: 4,
+        bookingAt: "2022/10/23",
         startAt: "09:00",
         endAt: "10:00",
         symptoms: "fever, headache",
