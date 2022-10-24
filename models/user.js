9dadeb38cc3a806f80b2084f28d0016cc364f939
Huishan <hui_shan91@hotmail.com>
2022-10-25 07:46:16 +0800

    feat: updated more changes to bookingAppt but not done

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.booking.belongsTo(models.user);
+      models.user.hasMany(models.booking);
     }
   }
   user.init(
