ed9d4d8c889a3b19ef504f55482a0a874fe2d499
Huishan <hui_shan91@hotmail.com>
2022-11-02 08:19:59 +0800

    feat: updated

--- models/index.js
+++ models/index.js
@@ -8,29 +8,29 @@ const basename = path.basename(__filename);
 // const env = process.env.NODE_ENV || "development";
 // const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
-// const config = require(__dirname + "/../config/config.js");
+const config = require(__dirname + "/../config/config.js");
 
-const sequelize = new Sequelize(process.env.DB_URL);
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error) => {
-    console.error("Unable to connect to the database: ", error);
-  });
+// const sequelize = new Sequelize(process.env.DB_URL);
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((error) => {
+//     console.error("Unable to connect to the database: ", error);
+//   });
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(
-//     config.database,
-//     config.username,
-//     config.password,
-//     config
-//   );
-// }
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
+}
 
 fs.readdirSync(__dirname)
   .filter((file) => {
